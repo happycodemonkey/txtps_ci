f8719fa005168073d4ca10e886f72e2e1b440c52
happycodemonkey <cmarnold2@gmail.com>
2012-04-10 17:13:56 -0500

    Created assets folder, added dropdown menu to header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,9 +1,53 @@
 <html>
-<head><title>TxTPS - Texas Test Problem Server</title>
+<head>
+<script type="text/javascript" src="assets/js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="assets/js/site.js"></script>
+<style src="assets/css/site.css"></style>
+<title>TxTPS - Texas Test Problem Server</title>
 </head>
 <body>
-[Top menu]
-<img src="assets/images/TxTPS.png" />
+<div class="header_menu">
+	<a href="http://tacc.utexas.edu"><img src="assets/images/tacc_s.png" /></a>
+	<a href="/about">About</a>
+	<a href="/contact">Contact</a>
+	<a href="/help">Help</a>
+	<a href="/login">Login</a>
+	<a href="/register">Register</a>
+</div>
+<img src="assets/images/TxTPS_s.png" />
 <h2>Texas Test Problem Server - Logo</h2>
-Menu will go here. 
+<ul id="nav">
+	<li>
+		<a href="#">Create</a>
+		<ul>
+			<li><a href="#">Collections</a></li>
+			<li><a href="#">Generators</a></li>
+			<li><a href="#">Finite Element</a></li>
+			<li><a href="#">Matrix Market</a></li>
+			<li><a href="#">Very Large</a></li>
+		</ul>
+		<div class="clear"></div>
+	</li>
+	<li>
+		<a href="#">Research</a>
+		<ul>
+			<li><a href="#">Stored Problems</a></li>
+			<li><a href="#">File Formats</a></li>
+			<li><a href="#">Using TxTPS</a></li>
+			<li><a href="#">Software</a></li>
+		</ul>
+		<div class="clear"></div>
+	</li>
+	<li>
+		<a href="#">Explore</a>
+		<ul>
+			<li><a href="#">About TxTPS</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">News</a></li>
+			<li><a href="#">FAQ</a></li>
+		</ul>
+		<div class="clear"></div>
+	</li>
+</ul>
+<div class="clear"></div>
 <hr />
---
+++ assets/js/site.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {	
+	
+	$('#nav li').hover(
+		function () {
+			//show its submenu
+			$('ul', this).slideDown(100);
+
+		}, 
+		function () {
+			//hide its submenu
+			$('ul', this).slideUp(100);			
+		}
+	);
+	
+});
