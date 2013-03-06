fc0c0af4aaefdc95a42149f13f3bd79d6dd4f182
happycodemonkey <cmarnold2@gmail.com>
2013-03-06 11:45:13 -0600

    Links in the admin menu were wrong

--- application/views/pages/admin_menu.php
+++ application/views/pages/admin_menu.php
@@ -1,8 +1,8 @@
 <div class='site_body'>
 <ul>
 	<li><a href="/users/manage">Users</a></li>
-	<li><a href="/collections/manage">Collections</a></li>
-	<li><a href="/generators/manage">Generators</a></li>
-	<li><a href="/problems/manage">Problems</a></li>
+	<li><a href="/collections/view">Collections</a></li>
+	<li><a href="/generators/view">Generators</a></li>
+	<li><a href="/problems/view">Problems</a></li>
 </ul>
 </div>
