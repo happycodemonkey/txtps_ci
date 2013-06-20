86efa972c2cb838c4b8a92aeb7c7ab398ebba96f
happycodemonkey <cmarnold2@gmail.com>
2013-06-20 15:49:43 -0500

    Minor typo fixed

--- application/controllers/problems.php
+++ application/controllers/problems.php
@@ -52,7 +52,7 @@
 				foreach (scandir('/data/files/problems/' . $problem_id . '/public') as $file) {
 					if (preg_match("/^.*\.pdf$/i", $file)) {
 						$data['pdfs'][] = $file;
-					} else if (preg_match("^.*\.png$/i", $file)) {
+					} else if (preg_match("/^.*\.png$/i", $file)) {
 						$data['pngs'][] = $file;					
 					} else {
 						$data['files'][] = $file;
